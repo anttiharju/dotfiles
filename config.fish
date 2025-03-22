8b9d3714533c7bd57051ea76bd05e7bb8c16b8a0
Antti Harju <antti@harju.io>
2025-03-22 16:30:48 +0200

    Remove flox autoactivate

--- config.fish
+++ config.fish
@@ -18,24 +18,4 @@ if status --is-interactive
   if test -f "$machineconfig"
     source "$machineconfig"
   end
-
-  # Activate flox upon VS Code integrated terminal initialisation
-  if git rev-parse --git-dir >/dev/null 2>&1
-    # In git repo
-    set GIT_ROOT (git rev-parse --show-toplevel)
-    if test -d "$GIT_ROOT/.flox"
-      set CURRENT (echo "$FLOX_ENV" | sed 's/\.flox.*/.flox/')
-      set AVAILABLE "$GIT_ROOT/.flox"
-      if test $CURRENT != $AVAILABLE
-        flox activate -d $GIT_ROOT
-      end
-    end
-  else if test -d ./.flox
-    # Local .flox fallback
-    set CURRENT (echo "$FLOX_ENV" | sed 's/\.flox.*/.flox/')
-    set AVAILABLE "$(pwd)/.flox"
-    if test $CURRENT != $AVAILABLE
-      flox activate -d .
-    end
-  end
 end
