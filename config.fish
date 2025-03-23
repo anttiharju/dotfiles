f296e37d1cd3cb3a6b7c45ca1ea713a3afe64766
Antti Harju <git@anttiharju.dev>
2025-03-23 09:17:04 +0200

    Move custom utils to interactive sessions only

--- config.fish
+++ config.fish
@@ -3,7 +3,6 @@ fish_vi_key_bindings
 fish_hybrid_key_bindings
 
 eval "$(/opt/homebrew/bin/brew shellenv)"
-set -gx PATH /Users/antti/anttiharju/scripts $PATH
 
 if test -z "$SSH_AUTH_SOCK"
   eval $(ssh-agent -c)
@@ -17,4 +16,5 @@ end
 if status --is-interactive
   alias kc="kubectl"
   alias tc="talosctl"
+  set -gx PATH /Users/antti/anttiharju/scripts $PATH
 end
