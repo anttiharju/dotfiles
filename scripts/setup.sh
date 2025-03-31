34b97c4e151686d61154885c7ef5dc787fab1c84
Antti Harju <antti.harju@smartly.io>
2025-03-31 12:43:32 +0300

    Make it ShellCheck -able

---
+++ scripts/setup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Todo: consider soft symlinks
+ln -f ./com.mitchellh.ghostty/config ~/Library/Application\ Support/com.mitchellh.ghostty/config
+ln -f ./.tmux.conf ~/.tmux.conf
+ln -f ./.aerospace.toml ~/.aerospace.toml
+ln -f ./config.fish ~/.config/fish/config.fish
+ln -f ./com.microsoft.VSCode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+ln -f ./.gitconfig ~/.gitconfig
+mkdir -p ~/.config/git
+ln -f ./.global.gitignore ~/.config/git/ignore
+[ -f "./machine/config.sh" ] && ./machine/config.sh || true
