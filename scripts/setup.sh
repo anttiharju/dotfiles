d6a3878c5ce1bff173c01735bd3ddd80465f1f01
Antti Harju <antti@harju.io>
2025-05-19 14:56:02 +0300

    Include tmux reload in setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,6 +3,7 @@
 # Todo: consider soft symlinks
 ln -f ./com.mitchellh.ghostty/config ~/Library/Application\ Support/com.mitchellh.ghostty/config
 ln -f ./.tmux.conf ~/.tmux.conf
+tmux source-file ~/.tmux.conf
 ln -f ./.aerospace.toml ~/.aerospace.toml
 ln -f ./config.fish ~/.config/fish/config.fish
 ln -f ./com.microsoft.VSCode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
